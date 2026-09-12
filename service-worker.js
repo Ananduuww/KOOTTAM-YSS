a6482f99f90c4c2f17dfb1983eea624d3d126112
Ananduuww <ananduksw@gmail.com>
2026-09-12 13:15:04 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='koottam-yss-v1';
-const SHELL=['./','./index.html','./manifest.json'];
+const CACHE='koottam-yss-v4';
+const SHELL=['/KOOTTAM-YSS/','/KOOTTAM-YSS/index.html','/KOOTTAM-YSS/manifest.json','/KOOTTAM-YSS/icons/icon-192.png','/KOOTTAM-YSS/icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/KOOTTAM-YSS/index.html'))))});
